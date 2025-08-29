660ce85e32d15ed3b574aa94d3a3e0005b3b0013
Vipul Pandey <vipul1998pandey@gmail.com>
2025-08-29 22:15:32 +0530

    updated

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: "https://chat-app-dxnu.onrender.com/", // "http://localhost:5500"
-    baseURL: "http://localhost:5500"
+    baseURL: "https://chat-app-dxnu.onrender.com/", // "http://localhost:5500"
+    // baseURL: "http://localhost:5500"
 });
 
 export default instance;
